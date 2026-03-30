0a122f8d8151716c22c25d5723f3f9314c9501ec
Rogerio <jo-rogerio@hotmail.com>
2026-03-30 18:33:32 -0300

    hfgfgfg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Controle Financeiro',
+        short_name: 'Controle',
+        description: 'App de controle financeiro simples e eficiente',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone', // 🔥 ESSENCIAL (remove URL)
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
