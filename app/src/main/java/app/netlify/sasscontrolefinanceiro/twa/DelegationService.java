2a8b1cb93a8c4897dc939ea07a99c6ad35bd80c2
Rogerio <jo-rogerio@hotmail.com>
2026-03-30 19:58:51 -0300

    hfhfhf

---
+++ app/src/main/java/app/netlify/sasscontrolefinanceiro/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.sasscontrolefinanceiro.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
